f5d82642b94247edb0e2fb15c143a76ff8e16fd1
Aleksei Smirnov <sibusky@yandex.ru>
2023-08-26 12:02:58 +0300

    feat: implement whole hero section without media queries

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -20,24 +20,45 @@ export function Hero() {
         </div>
         <div className='hero__summary'>
           <ul className='hero__summary-list'>
-            <li className='hero__summary-item'>
-              <Link to='/about' className='text'>
-                мы <span className='hero__span'>1</span> на рынке
+            <li className='hero__summary-item hero__summary-item-1'>
+              <Link to='/about' className='hero__summary-item-text text'>
+                мы
+                <br />
+                <br />
+                <span className='hero__span'>1</span>
+                <br />
+                на рынке
               </Link>
             </li>
-            <li className='hero__summary-item'>
-              <Link to='/guarantee' className='text'>
-                гарантируем <span className='hero__span'>50%</span> безопасность
+            <li className='hero__summary-item hero__summary-item-2'>
+              <Link to='/guarantee' className='hero__summary-item-text text'>
+                гарантируем
+                <br />
+                <br />
+                <span className='hero__span'>50%</span>
+                <br />
+                безопасность
               </Link>
             </li>
-            <li className='hero__summary-item'>
-              <Link to='/technology' className='text'>
-                календарик за <span className='hero__span'>2001г.</span> в подарок
+            <li className='hero__summary-item hero__summary-item-3'>
+              <Link to='/technology' className='hero__summary-item-text text'>
+                календарик за
+                <br />
+                <br />
+                <span className='hero__span'>
+                  2001<span className='hero__span_small'>г.</span>
+                </span>
+                <br />в подарок
               </Link>
             </li>
-            <li className='hero__summary-item'>
-              <Link to='/schedule' className='text'>
-                путешествие <span className='hero__span'>597</span> дней
+            <li className='hero__summary-item hero__summary-item-4'>
+              <Link to='/schedule' className='hero__summary-item-text text'>
+                путешествие
+                <br />
+                <br />
+                <span className='hero__span'>597</span>
+                <br />
+                дней
               </Link>
             </li>
           </ul>
